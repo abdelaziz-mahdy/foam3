5fb1b2ed1333327233379e3beffc560fd6d05058
Joel Hughes <joel@nanopay.net>
2024-11-11 20:00:17 -0500

    update for single host use

--- tools/bin/install-nobak.sh
+++ tools/bin/install-nobak.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Calls install_remote with backup false
 
+HOST=$1
 APP_NAME=$(./build.sh -XappName | grep "Application Name" | sed -E 's/(.*):{1}(.*)/\2/' | tr -d '[:blank:]')
 VERSION=$(./build.sh -Xversions | grep "Application Version" | sed -E 's/(.*):{1}(.*)/\2/' | tr -d '[:blank:]')
 TARBALL_NAME=${APP_NAME}-deploy-${VERSION}.tar.gz
@@ -10,9 +11,6 @@ mkdir -p ${TMP_PATH}
 cp build/package/${TARBALL_NAME} ${TMP_PATH}/
 TARBALL=${TMP_PATH}/${TARBALL_NAME}
 
-exec 4<$1
-while read -u4 m; do
-    echo $m
-    ssh -o ConnectTimeout=5 $m 'touch /tmp/OFFLINE; sleep 5; sudo systemctl stop foam; rm /tmp/OFFLINE'
-    tools/bin/install_remote.sh -Bfalse -W$m -T${TARBALL} &
-done
+echo $HOST
+ssh -o ConnectTimeout=5 $HOST 'touch /tmp/OFFLINE; sleep 5; sudo systemctl stop foam; rm /tmp/OFFLINE'
+foam3/tools/bin/install_remote.sh -Bfalse -W$HOST -T${TARBALL} &
