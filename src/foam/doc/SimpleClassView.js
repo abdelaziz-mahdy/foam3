ca06b9c860fe95bbaa7cbe8d23f135c386e4aa69
Kevin Greer <kgrgreer@gmail.com>
2025-01-12 20:35:05 -0500

    Extract common code to display an axiom section into a method.

--- src/foam/doc/SimpleClassView.js
+++ src/foam/doc/SimpleClassView.js
@@ -138,46 +138,32 @@ foam.CLASS({
           }).
         end().
 
-        add(this.AxiomSummaryView.create({
-          title: 'Property Summary',
-          of: this.PropertyAxiom,
-          modelId: model.id,
-        })).
-        forEach(exts.map(function(e) { return e.id }).concat(impls.map(function(i) { return i.path })), function(id) {
-          this.
-            start(this.AxiomListView, {
-              of: this.PropertyAxiom,
-              modelId: id,
-              titleFn: () => {
-                return this.E('h4').
-                  add('Properties inherited from ').
-                  tag(this.ClassLink, { data: id });
-              },
-            }).
-            end();
-        }).
-
-        add(this.AxiomSummaryView.create({
-          title: 'Method Summary',
-          of: this.MethodAxiom,
-          modelId: model.id,
-          sort: true
-        })).
-        forEach(exts.map(function(e) { return e.id }).concat(impls.map(function(i) { return i.path })), function(id) {
-          this.
-            start(this.AxiomListView, {
-              of: this.MethodAxiom,
-              modelId: id,
-              titleFn: () => {
-                return this.E('h4').
-                  add('Methods inherited from ').
-                  tag(this.ClassLink, { data: id });
-              },
-            }).
-            end();
-        })
-
+        call(this.addAxiomSection, [this, exts, model, impls, 'Property', 'Properties', this.PropertyAxiom]).
+        call(this.addAxiomSection, [this, exts, model, impls, 'Method',   'Methods',    this.MethodAxiom]);
+//        call(this.addAxiomSection, [this, exts, model, impls, 'Action',   'Actions',    this.ActionAxiom]);
         // TODO property and method detail sections.
+    },
+
+    function addAxiomSection(self, exts, model, impls, name, plural, of) {
+      this.add(self.AxiomSummaryView.create({
+        title:   name + ' Summary',
+        of:      of,
+        modelId: model.id,
+        sort:    true
+      })).
+      forEach(exts.map(function(e) { return e.id }).concat(impls.map(function(i) { return i.path })), function(id) {
+        self.
+          start(self.AxiomListView, {
+            of:      of,
+            modelId: id,
+            titleFn: () => {
+              return self.E('h4').
+                add(plural + ' inherited from ').
+                tag(self.ClassLink, { data: id });
+            },
+          }).
+          end();
+      });
     }
   ]
 });
