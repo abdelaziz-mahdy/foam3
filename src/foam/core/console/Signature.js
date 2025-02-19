240e67fd53efceeb9bc407b3a29ae702f2036740
Kevin Greer <kgrgreer@gmail.com>
2025-02-19 12:29:56 -0400

    Add Signature support to Console.

---
+++ src/foam/core/console/Signature.js
@@ -0,0 +1,96 @@
+/**
+ * @license
+ * Copyright 2025 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+
+foam.CLASS({
+  package: 'foam.core.console',
+  name: 'SignatureView',
+  extends: 'foam.u2.View',
+
+  css: `
+    ^ .foam-u2-ActionView {
+      margin-left: 24px;
+      height: 24px;
+    }
+  `,
+
+  methods: [
+    function render() {
+      this.
+        addClass().
+        add(this.data.label$, ' ').
+        start('span').
+          show(this.data.signor$).
+          style({border: '4px solid black', padding: '3px' }).
+          add(this.data.signor$).
+        end().
+        add(' ', this.data.timestamp$.map(ts => ts ? '' + ts : ''), ' ', this.data.SIGN, this.data.UNSIGN);
+      // TODO: I don't know why the timestamp$.map is required
+    }
+  ]
+});
+
+
+foam.CLASS({
+  package: 'foam.core.console',
+  name: 'Signature',
+//  extends: 'foam.u2.Controller',
+
+  properties: [
+    {
+      class: 'String',
+      name: 'label'
+    },
+    {
+      class: 'Boolean',
+      name: 'signed',
+      visibility: 'RO'
+    },
+    {
+      class: 'String',
+      name: 'signor',
+      visibility: 'RO'
+    },
+    {
+      class: 'DateTime',
+      name: 'timestamp',
+      visibility: 'RO'
+    },
+    {
+      class: 'String',
+      name: 'prerequisite'
+    },
+    {
+      class: 'String',
+      name: 'permission'
+    }
+  ],
+
+  methods: [
+    function addToE(e) {
+      e.tag(foam.core.console.SignatureView, {data: this});
+    }
+  ],
+
+  actions: [
+    {
+      name: 'sign',
+      isAvailable: function(signed, prerequisite) { return ! signed && ! prerequisite; },
+      code: function() {
+        this.signor    = 'Kevin Greer';
+        this.timestamp = new Date();
+        this.signed    = true;
+      }
+    },
+    {
+      name: 'unsign',
+      isAvailable: function(signed) { return signed; },
+      code: function() {
+        this.signor = this.timestamp = this.signed = undefined;
+      }
+    }
+  ]
+});
