c9774d69dc0304f6fd935e59b0d17a8492a3fa32
Kevin Greer <kgrgreer@gmail.com>
2025-05-27 15:03:27 -0400

    Add start of FileStore and MemoryStore.

---
+++ src/foam/dao/store/MemoryStore.js
@@ -0,0 +1,42 @@
+/**
+ * @license
+ * Copyright 2025 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+class MemoryStored
+  implements Stored
+{
+  protected final Object obj_;
+
+  public MemoryStored(Object obj) {
+    obj_ = obj;
+  }
+
+  public Object get() {
+    return obj_;
+  }
+}
+
+
+public class MemoryStore
+  implments Store
+{
+  protected Stored root_;
+
+  public MemoryStore() {
+  }
+
+  public Stored root() {
+    return root_;
+  }
+
+  public Stored storeRoot(Object obj) {
+    root_ = store(obj);
+    return root_;
+  }
+
+  public Stored store(Object obj) {
+    return new MemoryStored(obj);
+  }
+}
