54bfb136ded6508cb5c4db519acd7d3b1b37e8bf
Ajeet Gill <ajeetshergill@gmail.com>
2025-06-10 13:16:41 -0300

    shows richChoiceViewSimilar for all cSpecDAOs

--- src/foam/u2/DAOChoicesListView.js
+++ src/foam/u2/DAOChoicesListView.js
@@ -7,36 +7,55 @@
 foam.CLASS({
   package: 'foam.u2',
   name: 'DAOChoicesListView',
-  extends: 'foam.u2.view.ChoiceView',
+  extends: 'foam.u2.view.RichChoiceView',
+
+  requires: [
+    'foam.u2.view.RichChoiceViewSection',
+  ],
 
   imports: ['cSpecDAO'],
   
   properties: [
     'of',
     {
-      name: 'choices',
+      name: 'sections',
       value: []
     },
+    {
+      name: 'search',
+      value: true
+    },
+    {
+      name: 'choosePlaceholder',
+      value: 'Choose DAO...'
+    }
   ],
-
+  
   methods: [
     function init() {
-      var self = this;
-      var values = [];
-      
-      var allDAOs = this.cSpecDAO.where(
-        this.AND(
-          this.ENDS_WITH(foam.core.boot.CSpec.ID, 'DAO'),
-          this.EQ(foam.core.boot.CSpec.SERVE, true)
+      this.SUPER();
+
+      // var tmpDAO = foam.dao.MDAO.create({of: foam.core.boot.CSpec});
+      // tmpDAO.put(foam.core.boot.CSpec.create({
+      //   id: 'temp1',
+      //   name: 'temp1'
+      // }))
+
+      // var tmpSection = this.RichChoiceViewSection.create({
+      //   heading: 'Flow DAOs',
+      //   dao: tmpDAO
+      // });
+      var daoSection = this.RichChoiceViewSection.create({
+        heading: 'DAOs',
+        dao: this.cSpecDAO.where(
+          this.AND(
+            this.ENDS_WITH(foam.core.boot.CSpec.ID, 'DAO'),
+            this.EQ(foam.core.boot.CSpec.SERVE, true)
+          )
         )
-      );
-      
-      allDAOs.select().then(sink => {
-        sink.array.forEach(d => {
-          values.push([d, d.name]);
-        });
-        this.choices = [...values];
       });
+      this.sections = [daoSection];
+      // this.sections = [tmpSection, daoSection];
     }
   ]
 });
