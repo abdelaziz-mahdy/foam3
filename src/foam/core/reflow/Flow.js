a8a468ad4398963e076501319fc363296b287654
Abdelaziz Mahdy <Abdelaziz.h.mahdy@gmail.com>
2025-06-13 15:12:39 -0300

    refactor Flow.js - streamline memento handling and improve feedback mechanism

--- src/foam/core/reflow/Flow.js
+++ src/foam/core/reflow/Flow.js
@@ -109,25 +109,23 @@ foam.CLASS({
       hidden: true,
       transient: true,
       postSet: function(_, n) {
-        if ( this.feedback_ ) return;
-        this.feedback_ = true;
-        try {
-          // TODO: should still not output empty reactions_: or children:
-          var json = foam.json.Outputter.create({
-            pretty: true,
-            strict: true,
-            formatDatesAsNumbers: false,
-            outputDefaultValues: false,
-            useShortNames: false,
-            propertyPredicate: function(_, p) { return ! p.externalTransient && ! p.networkTransient; }
-          });
-          //          this.mementoStr = foam.json.Short.stringify(n);
+        var json = foam.json.Outputter.create({
+          pretty: true,
+          strict: true,
+          formatDatesAsNumbers: false,
+          outputDefaultValues: false,
+          useShortNames: false,
+          propertyPredicate: function(_, p) { return ! p.externalTransient && ! p.networkTransient; }
+        });
+
+
           // HACK: Console doesn't set name until after the block is added, so if we store the mementoStr
           // now it will lack the name. Just delay a bit to allow name to be set.
-          setTimeout(() => this.mementoStr = json.stringify(n),1);
-        } finally {
-          this.feedback_ = false;
-        }
+          setTimeout(() => {
+            this.lastMementoStrFromMemento_ = newStr;
+            this.mementoStr =  json.stringify(n);
+          }, 1);
+        
       }
     },
     {
@@ -155,19 +153,19 @@ foam.CLASS({
       name: 'mementoStr',
       label: 'Script',
       postSet: function(_, n) {
-        if ( this.feedback_ ) return;
-        this.feedback_ = true;
-        try {
-          // console.log('*********** FLOW mementoStr change:', n);
-          n = n.trim();
-          if ( n ) {
-            var json = JSON.parse(n);
-            this.memento = foam.json.parse(json, null, this.__context__);
-          } else {
-            this.memento = [];
-          }
-        } finally {
-          this.feedback_ = false;
+        // Don't update if this change came from memento postSet
+        if ( this.lastMementoStrFromMemento_ === n ) {
+          this.lastMementoStrFromMemento_ = null; // Clear the tracking
+          return;
+        }
+        
+        // console.log('*********** FLOW mementoStr change:', n);
+        n = n.trim();
+        if ( n ) {
+          var json = JSON.parse(n);
+          this.memento = foam.json.parse(json, null, this.__context__);
+        } else {
+          this.memento = [];
         }
       },
       view: { class: 'foam.u2.tag.TextArea', rows: 8, cols: 78 }
