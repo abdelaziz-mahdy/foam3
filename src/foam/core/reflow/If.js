0011ce2e1a75cc17afa0e2c7902f87762eac2b9c
Abdelaziz Mahdy <Abdelaziz.h.mahdy@gmail.com>
2025-06-16 14:30:10 -0300

    Add conditional execution class and commands for 'If' logic

---
+++ src/foam/core/reflow/If.js
@@ -0,0 +1,40 @@
+/**
+ * @license
+ * Copyright 2025 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.core.reflow',
+  name: 'If',
+
+  imports: [
+    'eval_'
+  ],
+
+  properties: [
+    {
+      class: 'String',
+      name: 'condition'
+    },
+    {
+      class: 'String',
+      name: 'ifBlock'
+    },
+    {
+      class: 'String',
+      name: 'elseBlock'
+    }
+  ],
+
+  methods: [
+    function addToE(e) {
+      var conditionResult = this.eval_(this.condition);
+      var scriptToExecute = conditionResult ? this.ifBlock : this.elseBlock;
+      
+      if (scriptToExecute) {
+        this.eval_(scriptToExecute);
+      }
+    }
+  ]
+});
