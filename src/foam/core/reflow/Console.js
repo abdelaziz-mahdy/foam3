d695c98918d9c9bc7fdaae8cfec022dd66274684
Kevin Greer <kgrgreer@gmail.com>
2025-07-01 11:52:50 -0300

    Reflow Script objects now bind to the scope properly.

--- src/foam/core/reflow/Console.js
+++ src/foam/core/reflow/Console.js
@@ -1137,7 +1137,9 @@ foam.CLASS({
 
       // Add bindings for children
       this.flowChildren.forEach(c => {
-        if ( c.value ) s[c.flowName] = c.value;
+        if ( c.value ) {
+          s[c.flowName] = foam.lang.Holder.isInstance(c.value) ? c.value.value : c.value;
+        }
       });
     },
 
