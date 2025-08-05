6ab8742d72983a2712cbae454075de21dfbaeff6
Kevin Greer <kgrgreer@gmail.com>
2025-08-04 21:11:32 -0300

    Fix indentation.

--- src/foam/core/reflow/control/CollectionsControl.js
+++ src/foam/core/reflow/control/CollectionsControl.js
@@ -5,28 +5,27 @@
  */
 
 foam.CLASS({
-    package: 'foam.core.reflow.control',
-    name: 'CollectionsControl',
-    extends: 'foam.core.reflow.control.ComponentsControl',
-  
-    requires: [ 'foam.core.reflow.DynamicReflowData' ],
-  
-    properties: [
-      {
-        name: 'buttonLabel',
-        value: 'Collections'
-      },
-      {
-        name: 'buttonIcon',
-        value: 'file'
-      },
-      {
-        class: 'foam.u2.ViewSpec',
-        name: 'spec',
-        factory: function() {
-          return { class: 'foam.core.reflow.DynamicReflowData', header: this.buttonLabel, dataType: 'collections' }
-        }
+  package: 'foam.core.reflow.control',
+  name: 'CollectionsControl',
+  extends: 'foam.core.reflow.control.ComponentsControl',
+
+  requires: [ 'foam.core.reflow.DynamicReflowData' ],
+
+  properties: [
+    {
+      name: 'buttonLabel',
+      value: 'Collections'
+    },
+    {
+      name: 'buttonIcon',
+      value: 'file'
+    },
+    {
+      class: 'foam.u2.ViewSpec',
+      name: 'spec',
+      factory: function() {
+        return { class: 'foam.core.reflow.DynamicReflowData', header: this.buttonLabel, dataType: 'collections' }
       }
-    ]
-  });
-  
+    }
+  ]
+});
