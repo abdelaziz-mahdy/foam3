45d2b585af4c115631ece8949e2864a4b84d6c85
Kevin Greer <kgrgreer@gmail.com>
2025-10-02 20:07:24 +0200

    Fix Reference type search filters not working on client-side DAOs.

--- src/foam/lang/types.js
+++ src/foam/lang/types.js
@@ -1121,9 +1121,11 @@ foam.CLASS({
     {
       name: 'adapt',
       value: function(oldValue, newValue, prop) {
-        return (prop || this).of.isInstance(newValue) ?
-          newValue.id :
-          newValue ;
+        let of = (prop || this).of;
+        if ( of.isInstance(newValue) ) return newValue.id;
+        if ( foam.lang.MultiPartID.isInstance(of.ID) ) return newValue;
+        if ( ! of.ID.adapt ) return newValue;
+        return of.ID.adapt.call(this, newValue);
       }
     },
     {
