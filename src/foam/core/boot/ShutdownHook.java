b4f1d526ad7f2e7c4fb64746020da83d9c45dadf
Joel Hughes <joel@nanopay.net>
2025-11-24 19:20:42 -0500

    ShutdownHook - revert explicit remaining user threadinterrupts, not required

--- src/foam/core/boot/ShutdownHook.java
+++ src/foam/core/boot/ShutdownHook.java
@@ -79,17 +79,6 @@ public class ShutdownHook
       }
     }
 
-    logger.debug("Shutdownhook,shutdown,interrupt","start");
-    for (Thread thread : Thread.getAllStackTraces().keySet()) {
-      if (thread != Thread.currentThread() &&
-          ! thread.isDaemon() &&
-          ! thread.isInterrupted() ) {
-        logger.debug("Shutdownhook,shutdown,interrupt,thread",thread.getName());
-        thread.interrupt();
-      }
-    }
-    logger.debug("Shutdownhook,shutdown,interrupt","end");
-
     logger.info("ShutdownHook,shutdown complete");
   }
 }
