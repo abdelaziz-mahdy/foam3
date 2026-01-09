86654d725a37d50188dd7117ca502a9269a364a5
Abdelaziz Mahdy <Abdelaziz.h.mahdy@gmail.com>
2026-01-09 17:17:09 -0400

    fix: update NumberParserTest flags to include java&test

--- src/pom.js
+++ src/pom.js
@@ -437,7 +437,7 @@ foam.POM({
     { name: "foam/parse/test/QueryParserUserTest",                    flags: "js&test|java&test" },
     { name: "foam/parse/test/SimpleQueryParserTest",                  flags: "js&test|java&test" },
     { name: "foam/parse/test/DateParserTest",                         flags: "js&test|java&test" },
-    { name: "foam/parse/test/NumberParserTest",                      flags: "js&test" },
+    { name: "foam/parse/test/NumberParserTest",                      flags: "js&test|java&test" },
     { name: "foam/physics/Physical",                                  flags: "js" },
     { name: "foam/physics/Collider",                                  flags: "js" },
     { name: "foam/physics/PhysicsEngine",                             flags: "js" },
