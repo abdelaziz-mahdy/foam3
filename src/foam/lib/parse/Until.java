ea29404be508996d1ad63aca6b2a8b4025a9d6f9
Abdelaziz Mahdy <Abdelaziz.h.mahdy@gmail.com>
2026-04-14 14:46:34 -0300

    Revert "feat(parse): add UntilLiteral/UntilLiteral0 for transparent indexOf optimization in until/until0"
    This reverts commit 917d6a0227ad8e1ddc9dd3f408b95b9cfc33a995.

--- src/foam/lib/parse/Until.java
+++ src/foam/lib/parse/Until.java
@@ -1,3 +1,4 @@
+
 /**
  * @license
  * Copyright 2020 The FOAM Authors. All Rights Reserved.
@@ -6,31 +7,21 @@
 
 package foam.lib.parse;
 
-/**
- * Matches any characters until the terminating pattern, returning an array of
- * the characters before the terminator. Consumes the terminator. Fails if the
- * terminator is never found.
- *
- * When the terminator is an AbstractLiteral, delegates to UntilLiteral which
- * uses String.indexOf() for O(n) native performance.
- */
 public class Until
-  implements Parser
-{
-  protected Parser delegate_;
+  implements Parser {
+
+  protected Parser until_;
 
   public Until(Parser until) {
-    if ( until instanceof AbstractLiteral ) {
-      delegate_ = new UntilLiteral(((AbstractLiteral) until).getString());
-    } else {
-      delegate_ = new Seq1(0,
-        new Repeat(new Not(until, AnyChar.instance())),
-        until
-      );
-    }
+    // TODO: Make faster, and also add a UntilStr which builds a string directly
+    until_ = new Seq1(0,
+      new Repeat(new Not(until, AnyChar.instance())),
+      until
+    );
   }
 
   public PStream parse(PStream ps, ParserContext x) {
-    return delegate_.parse(ps, x);
+    PStream pst = until_.parse(ps, x);
+    return pst;
   }
 }
